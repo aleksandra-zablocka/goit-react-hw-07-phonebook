36297bb750a0b20463224399b7aac76045199cc3
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-09-23 17:33:20 +0200

    test

--- src/components/ContactEl/ContactEl.jsx
+++ src/components/ContactEl/ContactEl.jsx
@@ -11,7 +11,7 @@ const handleRemove = () => {
 
 return (
   <div className={css.contactEl}>
-    <p>{contact.name}</p>
+    <p>{contact.name}a</p>
     <p>{contact.number}</p>
     <button className={css.removeBtn} onClick={handleRemove}>Remove</button>
   </div>
